f3c57afb8c901f0125ca3e78f62aa91d848c9117
Sheng Xu <u5538588@anu.edu.au>
2017-08-28 17:04:24 +1000

    T4 updated

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -54,48 +54,50 @@ public class Viewer extends Application {
         // FIXME Task 4: implement the simple placement viewer
 
         shapes.getChildren().clear();
-        if(!StepsGame.isPlacementWellFormed(placement)){
-            throw new IllegalArgumentException("Bad Placement: " + placement);
+        if(!StepsGame.isPlacementSequenceValid(placement)){
+            System.out.println("Invalid placement: "+placement);
+            System.out.println("Please try again!");
+            //throw new IllegalArgumentException("Bad Placement: " + placement);
         }
-        char[] pieces = placement.toCharArray();
-        char[] Mask = new char[pieces.length/3];
-        char[] Rot = new char[pieces.length/3];
-        char[] Pos = new char[pieces.length/3];
-        int j=0;
-        int[] Pos_index = new int[pieces.length/3];
-        int[] row_index = new int[pieces.length/3];
-        int[] rotDegree = new int[pieces.length/3];
-        ImageView[] pc = new ImageView[pieces.length/3];
-        for(int i = 0; i < pieces.length; i=i+3){
-            Mask[j] = pieces[i];
-            Rot[j] = pieces[i+1];
-            if(Rot[j]>='A' && Rot[j]<'E'){
-                rotDegree[j] = 90*(Rot[j]-'A');
-                Rot[j] = 'A';
+        else {
+            char[] pieces = placement.toCharArray();
+            char[] Mask = new char[pieces.length / 3];
+            char[] Rot = new char[pieces.length / 3];
+            char[] Pos = new char[pieces.length / 3];
+            int j = 0;
+            int[] Pos_index = new int[pieces.length / 3];
+            int[] row_index = new int[pieces.length / 3];
+            int[] rotDegree = new int[pieces.length / 3];
+            ImageView[] pc = new ImageView[pieces.length / 3];
+            for (int i = 0; i < pieces.length; i = i + 3) {
+                Mask[j] = pieces[i];
+                Rot[j] = pieces[i + 1];
+                if (Rot[j] >= 'A' && Rot[j] < 'E') {
+                    rotDegree[j] = 90 * (Rot[j] - 'A');
+                    Rot[j] = 'A';
+                } else if (Rot[j] >= 'E' && Rot[j] <= 'H') {
+                    rotDegree[j] = 90 * (Rot[j] - 'A');
+                    Rot[j] = 'E';
+                }
+                Pos[j] = pieces[i + 2];
+                Pos_index[j] = StepsGame.BOARD_STRING.indexOf(Pos[j]);
+                row_index[j] = Pos_index[j] / 10;
+                Pos_index[j] = Pos_index[j] % 10;
+                j++;
             }
-            else if(Rot[j]>='E' && Rot[j]<='H'){
-                rotDegree[j] = 90*(Rot[j]-'A');
-                Rot[j] = 'E';
+            for (int i = 0; i < pieces.length / 3; i++) {
+                piece = new Image(URI_BASE2 + Character.toString(Mask[i]) + Character.toString(Rot[i]) + ".png");
+                pc[i] = new ImageView();
+                pc[i].setImage(piece);
+                pc[i].setFitWidth(PIECE_IMAGE_SIZE);
+                pc[i].setFitHeight(PIECE_IMAGE_SIZE);
+                pc[i].setX(MARGIN_X + SQUARE_SIZE * Pos_index[i] - SQUARE_SIZE);
+                pc[i].setY(MARGIN_Y + SQUARE_SIZE * row_index[i] - SQUARE_SIZE);
+                pc[i].setOpacity(0.75f);
+                pc[i].setRotate(rotDegree[i]);
+                shapes.getChildren().add(pc[i]);
             }
-            Pos[j] = pieces[i+2];
-            Pos_index[j] = StepsGame.BOARD_STRING.indexOf(Pos[j]);
-            row_index[j] = Pos_index[j]/10;
-            Pos_index[j] = Pos_index[j]%10;
-            j++;
         }
-        for(int i =0;i<pieces.length/3;i++){
-            piece = new Image(URI_BASE2+Character.toString(Mask[i])+Character.toString(Rot[i])+".png");
-            pc[i] = new ImageView();
-            pc[i].setImage(piece);
-            pc[i].setFitWidth(PIECE_IMAGE_SIZE);
-            pc[i].setFitHeight(PIECE_IMAGE_SIZE);
-            pc[i].setX(MARGIN_X+SQUARE_SIZE*Pos_index[i]-SQUARE_SIZE);
-            pc[i].setY(MARGIN_Y+SQUARE_SIZE*row_index[i]-SQUARE_SIZE);
-            pc[i].setOpacity(0.75f);
-            pc[i].setRotate(rotDegree[i]);
-            shapes.getChildren().add(pc[i]);
-        }
-        //controls.getChildren().add(shapes);
     }
 
     /**
@@ -112,7 +114,6 @@ public class Viewer extends Application {
             pc[i].setX(15+SQUARE_SIZE*i*0.75);
             pc[i].setY(10);
             initializor.getChildren().add(pc[i]);
-            //controls.getChildren().add(pc[i]);
         }
     }
 
@@ -138,7 +139,7 @@ public class Viewer extends Application {
             @Override
             public void handle(ActionEvent e) {
                 makePlacement(textField.getText());
-                textField.clear();
+                //textField.clear();
             }
         });
         HBox hb = new HBox();
