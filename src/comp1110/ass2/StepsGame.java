51bee5773d89f1646d314da8b56503231baa8c00
TOTO <TOTO@TOTOs-MacBook-Pro.local>
2017-08-28 23:20:31 +1000

    Revert "Added own Test For T5 AAWBBi"
    This reverts commit 45af26a

--- src/comp1110/ass2/StepsGame.java
+++ src/comp1110/ass2/StepsGame.java
@@ -286,10 +286,6 @@ public class StepsGame {
             return false;
         }
     }
-    @Test
-    public void testT5_AAWBBi_false(){
-        assertFalse(isPlacementSequenceValid("AAWBBi"));
-    }
 
     /**
      * Given a string describing a placement of pieces and a string describing
