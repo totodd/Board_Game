262b2e9adf900c1505b97d067712c95c402eaa92
Tao Chen <tao@quizlingapp.com>
2017-09-28 10:11:34 +1000

    originality

--- src/comp1110/ass2/test/T5test.java
+++ src/comp1110/ass2/test/T5test.java
@@ -2,40 +2,53 @@ package comp1110.ass2.test;
 
 import comp1110.ass2.Place;
 import comp1110.ass2.StepsGame;
+import org.junit.Test;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
 import java.util.List;
 
 
 
 public class T5test {
-    public static void main(String[] args) {
 
-        String s = "EHu";
-        String false_s = "AAWBBi";
-
+    @Test
+    public void onRightLevel(){
+        String s = "Ehu";
         List<Place> p = StepsGame.turnToPlace(s);
-
-
-//        for (int i=0;i< p.get(0).getBoardIdx().length;i++){
-//            System.out.println(((p.get(0).getBoardIdx()[i] >= 50)&(p.get(0).getValue()[i]>0)));
-//        }
-//        for (int i=0;i< p.get(0).getBoardIdx().length;i++){
-//            System.out.println(((p.get(0).getBoardIdx()[i] < 0)&(p.get(0).getValue()[i]>0)));
-//        }
-//        for (int i=0;i< p.get(0).getBoardIdx().length;i++){
-//            System.out.println(((p.get(0).getBoardIdx()[i] < 0)&(p.get(0).getValue()[i]>0))
-//                    | ((p.get(0).getBoardIdx()[i] >= 50)&(p.get(0).getValue()[i]>0)));
-//        }
-
-        System.out.println("On right level: "+ StepsGame.onRightLevel(p.get(0)));
-        System.out.println("center: " + p.get(0).getCenter());
-        System.out.println("Within board: " + StepsGame.withInBoard(p.get(0)));
-//        System.out.println();
-//        for (int i : p.get(0).getValue()) {
-//            System.out.println(i);
-//        }
-
-        System.out.println( StepsGame.isPlacementSequenceValid(s));
-
+        assertTrue(StepsGame.onRightLevel(p.get(0)));
     }
+
+    @Test
+    public void onWrongLevel(){
+        String s = "AAh";
+        List<Place> p = StepsGame.turnToPlace(s);
+        assertFalse(StepsGame.onRightLevel(p.get(0)));
+    }
+
+    @Test
+    public void isSequenceValid(){
+        String s = "DFOGGQEDIBAkFHn";
+        assertTrue(StepsGame.isPlacementSequenceValid(s));
+    }
+
+    @Test
+    public void isNotSequenceValid(){
+        String s = "AAWBBi";
+        assertFalse(StepsGame.isPlacementSequenceValid(s));
+    }
+
+    @Test
+    public void isNotWithinBoard(){
+        String s = "AAA";
+        assertFalse(StepsGame.isPlacementSequenceValid(s));
+    }
+
+    @Test
+    public void isWithinBoard(){
+        String s = "AAW";
+        assertTrue(StepsGame.isPlacementSequenceValid(s));
+    }
+
+
 }
