3a8831625edc9b2da804c415daa2878688399454
Edward <u6032167@anu.edu.au>
2017-10-20 01:03:50 +1100

    Added comment to generate

--- src/comp1110/ass2/Generate2.java
+++ src/comp1110/ass2/Generate2.java
@@ -5,16 +5,15 @@
 package comp1110.ass2;
 
 import java.util.ArrayList;
-
+/**
+ * This is another attempt to generate all solutions
+ */
 public class Generate2 {
     static final String BOARD_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxy";
     static final String PIECES = "ABCDEFGH";
     static final ArrayList<String> viableSingleSolutions = new ArrayList<>();
 
-
     public static void main(String[] args) throws Exception {
-
-
         String start[]=Utility.readFiletoStringArray("4/DABC.txt");
         for(int i=0;i<start.length;i++){
             System.out.println(start[i]);
